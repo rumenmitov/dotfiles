3ca5cb7532681159da83541120e15bc1f9afb4d0
Rumen <rumenmitov@protonmail.com>
2024-04-24 00:31:41 +0200

    fixing language change keybinds in hyprland

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -59,11 +59,25 @@
 (setq use-package-always-ensure t)
 
 (use-package haskell-mode)
+(use-package go-mode)
 
 (add-hook 'haskell-mode-hook 'eglot-ensure)
+(add-hook 'go-mode-hook 'eglot-ensure)
 (add-hook 'c-mode-hook 'eglot-ensure)
 (add-hook 'c++-mode-hook 'eglot-ensure)
 
 (require 'flymake)
 (define-key flymake-mode-map (kbd "C-x M-]") 'flymake-goto-next-error)
 (define-key flymake-mode-map (kbd "C-x M-[") 'flymake-goto-prev-error)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(go-mode haskell-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
