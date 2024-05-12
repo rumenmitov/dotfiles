1afd4463482244336e108df90ae13294232d26ed
Rumen <rumenmitov@protonmail.com>
2024-05-12 23:58:38 +0200

    xmonad readme

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -11,7 +11,6 @@ import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Renamed
 import XMonad.Actions.CycleWS
 
-
 _startupHook :: X ()
 _startupHook = do
   spawnOnce "xrandr --output eDP --primary --mode 1920x1200"
@@ -20,40 +19,39 @@ _startupHook = do
   spawnOnce "xsetroot -cursor_name left_ptr"
   spawnOnce "xinput --set-prop 9 310 0.8"
   spawnOnce "greenclip daemon"
-  
+  spawnOnce "emacs --daemon"
 
 _xmobarPP :: PP
 _xmobarPP = def
-    { ppSep             = magenta " • "
-    , ppTitleSanitize   = xmobarStrip
-    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
-    , ppHidden          = white . wrap " " ""
-    , ppHiddenNoWindows = lowWhite . wrap " " ""
-    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
-    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
-    , ppExtras          = [logTitles formatFocused formatUnfocused]
-    }
-  where
-    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
-    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+  { ppSep             = magenta " • "
+  , ppTitleSanitize   = xmobarStrip
+  , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+  , ppHidden          = white . wrap " " ""
+  , ppHiddenNoWindows = lowWhite . wrap " " ""
+  , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+  , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+  , ppExtras          = [logTitles formatFocused formatUnfocused]
+  }
+    where
+      formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+      formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
 
-    ppWindow :: String -> String
-    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+      ppWindow :: String -> String
+      ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
 
-    blue, lowWhite, magenta, red, white, yellow :: String -> String
-    magenta  = xmobarColor "#ff79c6" ""
-    blue     = xmobarColor "#bd93f9" ""
-    white    = xmobarColor "#f8f8f2" ""
-    yellow   = xmobarColor "#f1fa8c" ""
-    red      = xmobarColor "#ff5555" ""
-    lowWhite = xmobarColor "#bbbbbb" ""
-
+      blue, lowWhite, magenta, red, white, yellow :: String -> String
+      magenta  = xmobarColor "#ff79c6" ""
+      blue     = xmobarColor "#bd93f9" ""
+      white    = xmobarColor "#f8f8f2" ""
+      yellow   = xmobarColor "#f1fa8c" ""
+      red      = xmobarColor "#ff5555" ""
+      lowWhite = xmobarColor "#bbbbbb" ""
 
 _layoutHook = renamed [Replace ""] $  ThreeCol tiles_in_master delta master_ratio
   where
-    tiles_in_master  = 1
-    delta            = 3/100
-    master_ratio     = 1/2
+    tiles_in_master  = 1         -- number of master tiles
+    delta            = 3/100     -- by how much to change the size of the tile
+    master_ratio     = 1/2       -- master size compared to screen
 
 _keybinds =
 
@@ -99,7 +97,7 @@ xmonadConfig = def
   , layoutHook         = _layoutHook
   , terminal           = "alacritty"
   }
-  
+
 
 main :: IO ()
 main = xmonad
@@ -112,5 +110,3 @@ main = xmonad
         toggleStrutsKey XConfig{ modMask = m } = (m, xK_d)
         keybinds                               = additionalKeysP remove _keybinds
         remove                                 = removeKeysP xmonadConfig _removeKeybinds
-
-  
