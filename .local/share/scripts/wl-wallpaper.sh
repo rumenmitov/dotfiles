54faade6ad7e47cca852f6d0f3833e3c77438ce6
Rumen <rumenmitov@protonmail.com>
2024-08-19 23:43:43 +0200

    minor upgrades

--- .local/share/scripts/wl-wallpaper.sh
+++ .local/share/scripts/wl-wallpaper.sh
@@ -1,4 +1,9 @@
 type=$( echo -e "Static\nLive" | rofi -dmenu | awk '{print tolower($1)}' )
+
+if [ -z "$type" ]; then
+    exit
+fi 
+
 wallpaper=$( ls "$HOME/.local/share/wallpapers/$type/" | rofi -dmenu )
 
 wallpaper_path="$HOME/.local/share/wallpapers/$type/$wallpaper"
