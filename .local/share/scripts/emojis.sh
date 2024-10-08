fc3f5f2ead12a11a4a8ac92033372220f284580a
Rumen <rumenmitov@protonmail.com>
2024-10-08 21:21:37 +0200

    fixing tr in clipboard scripts

--- .local/share/scripts/emojis.sh
+++ .local/share/scripts/emojis.sh
@@ -4,5 +4,5 @@ cat ~/.local/share/emojis/emojis.csv                                 \
     | dmenu                                                          \
     -i -l 4 -sb \#000000 -sf \#d000ff -fn 'Hack Nerd Font Mono 18'   \
     | awk '{print $1}'                                               \
-    | tr -d \"\n\r\t\"                                               \
+    | tr -d "\\n\\r\\t"                                              \
     | xclip -selection c
