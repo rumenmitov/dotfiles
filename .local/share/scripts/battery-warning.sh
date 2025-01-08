bdfd55a8c32d42c7de8017390138a77752fe8683
Rumen <rumenmitov@protonmail.com>
2025-01-08 13:44:16 +0100

    script: battery warning now checks if charging

--- .local/share/scripts/battery-warning.sh
+++ .local/share/scripts/battery-warning.sh
@@ -2,8 +2,9 @@
 
 while true; do
     battery=$(cat /sys/class/power_supply/BAT0/capacity)
+    connected=$( cat /sys/class/power_supply/AC0/online )
 
-    if [[ $battery -le 15 ]]; then 
+    if [ $battery -le 15 ] && [ $connected != "Charging" ]; then 
         dunstify "Low Battery" "Battery is at 15%!" -u critical -I \
             "/home/rumen/.local/share/scripts/icons/low-battery.png"
     fi
