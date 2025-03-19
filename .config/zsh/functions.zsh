d32cd20ee2a0c280205b416abae0f32fa9295e3e
Rumen <rumenmitov@protonmail.com>
2025-03-19 07:37:46 +0100

    zsh keepassxc function

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -2,3 +2,9 @@ function gac() {
     git add .
     git commit -S -m $1
 }
+
+function kpass() {
+    local DB=$HOME/Nextcloud/other/passwords.kdbx
+    local ENTRY=$(keepassxc-cli search $DB $1 | head -n 1)
+    test -z $ENTRY || keepassxc-cli clip $DB $ENTRY
+}
