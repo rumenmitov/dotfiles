1da6d91e01f360d72c00ed8c04ea18ab60c4426c
Rumen <rumenmitov@protonmail.com>
2025-07-08 14:42:30 +0200

    wezterm: configuring keybinds for multiplexing

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -24,11 +24,30 @@ config = {
     bottom = 0,
   },
 
-  enable_tab_bar = false,
+  enable_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
 
   window_close_confirmation = 'NeverPrompt',
 
+  disable_default_key_bindings = true,
   keys = {
+    { key = 'Tab', mods = 'ALT', action = wezterm.action.ActivateTabRelative(1) },
+    { key = 't', mods = 'ALT', action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+    { key = '1', mods = 'ALT', action = wezterm.action.ActivateTab(0) },
+    { key = '2', mods = 'ALT', action = wezterm.action.ActivateTab(1) },
+    { key = '3', mods = 'ALT', action = wezterm.action.ActivateTab(2) },
+    { key = '4', mods = 'ALT', action = wezterm.action.ActivateTab(3) },
+    { key = '5', mods = 'ALT', action = wezterm.action.ActivateTab(4) },
+    { key = '6', mods = 'ALT', action = wezterm.action.ActivateTab(5) },
+    { key = '7', mods = 'ALT', action = wezterm.action.ActivateTab(6) },
+    { key = '8', mods = 'ALT', action = wezterm.action.ActivateTab(7) },
+    { key = '9', mods = 'ALT', action = wezterm.action.ActivateTab(8) },
+    { key = '0', mods = 'ALT', action = wezterm.action.ActivateTab(9) },
+    { key = '+', mods = 'CTRL|SHIFT', action = wezterm.action.IncreaseFontSize },
+    { key = '-', mods = 'CTRL', action = wezterm.action.DecreaseFontSize },
+    { key = '=', mods = 'CTRL', action = wezterm.action.ResetFontSize },
+    { key = 'c', mods = 'CTRL|SHIFT', action = wezterm.action.CopyTo("Clipboard") },
+    { key = 'v', mods = 'CTRL|SHIFT', action = wezterm.action.PasteFrom("Clipboard") },
     { key = 'v', mods = 'ALT', action = wezterm.action.ScrollByPage(-1) },
     { key = 'v', mods = 'CTRL|ALT', action = wezterm.action.ScrollByPage(1) }
   },
@@ -40,7 +59,6 @@ config = {
       mods = 'NONE',
     },
   }
-
 }
 
 
