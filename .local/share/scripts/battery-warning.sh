62eda87da08e1dae3108ed9224399c6e90c97202
Rumen <rumenmitov@protonmail.com>
2025-07-08 12:46:11 +0200

    scripts: battery warning updated check whether laptop is running on AC

--- .local/share/scripts/battery-warning.sh
+++ .local/share/scripts/battery-warning.sh
@@ -3,7 +3,7 @@
 battery=$(cat /sys/class/power_supply/BAT0/capacity)
 connected=$( cat /sys/class/power_supply/AC0/online )
 
-if [ $battery -le 15 ] && [ $connected != "Charging" ]; then 
+if [ $battery -le 15 ] && [ $connected -eq 0 ]; then 
   dunstify "Low Battery" "Battery is at 15%!" -u critical -I \
     "$HOME/.local/share/scripts/icons/low-battery.png"
 fi
