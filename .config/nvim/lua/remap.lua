0893bf6e319b60637171b0a9bbd245442c874b4f
Rumen <rumenmitov@protonmail.com>
2025-07-09 10:43:34 +0200

    nvim: quit nvim if last buffer is closed

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -30,8 +30,19 @@ vim.keymap.set("n", "<leader>o", "<esc>:e ")
 -- Buffers
 vim.keymap.set("n", "<C-p>", ":bp<CR>")
 vim.keymap.set("n", "<C-n>", ":bn<CR>")
-vim.keymap.set("n", "<C-q>", ":bd!<CR>")
 vim.keymap.set("n", "<C-s>", ":up!<CR>")
+
+vim.keymap.set("n", "<C-q>", function()
+  vim.cmd("bd!")
+
+  local buffers = vim.fn.getbufinfo({ buflisted = 1 }) 
+
+  if #buffers == 1 then
+    if buffers[1]["name"] == "" then
+      vim.cmd("quit")
+    end
+  end
+end, { silent = true })
 
 -- LSP
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
