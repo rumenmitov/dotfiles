a6a992869ac63be5f4b397d13ea84e87e5f6246e
Rumen <rumenmitov@protonmail.com>
2025-07-29 13:05:54 +0200

    scripts: battery warning typo

--- .local/share/scripts/battery-warning.sh
+++ .local/share/scripts/battery-warning.sh
@@ -4,7 +4,7 @@ battery=$(cat /sys/class/power_supply/BAT0/capacity)
 connected=$( cat /sys/class/power_supply/AC0/online )
 
 if [ $battery -le 15 ] && [ $connected -eq 0 ]; then 
-  dunstify "Low Battery" "Battery is at 15%!" -u critical -I \
+  dunstify "Low Battery" "Battery is at $battery%!" -u critical -I \
     "$HOME/.local/share/scripts/icons/low-battery.png"
 fi
 
