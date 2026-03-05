850a40cfcf4440200db2b70c648d7591a0a43fca
Rumen <rumenmitov@protonmail.com>
2026-03-05 13:03:08 +0100

    scripts: live-wallpaper.sh early exit

--- .local/share/scripts/live-wallpaper.sh
+++ .local/share/scripts/live-wallpaper.sh
@@ -4,7 +4,9 @@ wallpapersdir="$HOME/.local/share/wallpapers/Live"
 
 wallpaper=$( ls "$wallpapersdir" | wofi --dmenu --prompt "🖼️" )
 
-wallpaper_path="$wallpapersdir/$type/$wallpaper"
+[ -z "$wallpaper" ] && exit 1
+
+wallpaper_path="$wallpapersdir/$wallpaper"
 
 pkill mpvpaper
 mpvpaper -s -o "no-audio --loop --panscan=1" ALL "$wallpaper_path" 
