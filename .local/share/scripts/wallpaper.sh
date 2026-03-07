1660dd237c54a8e414258e2c5ba7d7f98147637e
Rumen <rumenmitov@protonmail.com>
2026-03-07 20:33:55 +0100

    wofi: specific css for wallpaper menu

--- .local/share/scripts/wallpaper.sh
+++ .local/share/scripts/wallpaper.sh
@@ -2,17 +2,18 @@
 
 wallpapersdir="$HOME/.local/share/wallpapers/Static"
 
-wallpaper=$( ls "$wallpapersdir"                                    \
-  | xargs -I{} printf "img:%s/%s\n" "$wallpapersdir" {}             \
-  | wofi --dmenu --columns 3                                        \
-    --define "x=50px"                                               \
-    --define "y=25px"                                               \
-    --define "width=1200px"                                         \
-    --define "height=800px"                                         \
-    --define "image_size=300"                                       \
-    --allow-images                                                  \
-  | awk -F: '{ print $NF }'                                         \
-  | xargs basename)
+wallpaper=$( ls "$wallpapersdir"                                                      \
+  | xargs -I{} printf "img:%s/%s\n" "$wallpapersdir" {}                               \
+  | wofi --dmenu --columns 3                                                          \
+    --style="${XDG_CONFIG_HOME:-$HOME/.config}/wofi/wallpaper-style.css"              \
+    --define "x=50px"                                                                 \
+    --define "y=25px"                                                                 \
+    --define "width=1200px"                                                           \
+    --define "height=800px"                                                           \
+    --define "image_size=300"                                                         \
+    --allow-images                                                                    \
+  | awk -F: '{ print $NF }'                                                           \
+  | xargs basename )
 
 test -z "$wallpaper" && exit
 
