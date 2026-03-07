375f38e86e8e745e2651484e8896747d1c0d3cdd
Rumen <rumenmitov@protonmail.com>
2026-03-07 15:06:20 +0100

    dotfiles: clarified installation instructions

--- shell.nix
+++ shell.nix
@@ -5,6 +5,14 @@ pkgs.mkShell {
   shellHook = ''
   mkdir -p ~/.config ~/.local/share
   stow --ignore="shell\.nix" .
+
+  read -p "Do you want to enable the mail service?[y/N] " enable_mail
+
+  if [ $enable_mail = "y" ]; then
+    systemctl --user enable mail.timer
+    systemctl --user start mail.timer
+  fi
+
   exit
   '';
 }
