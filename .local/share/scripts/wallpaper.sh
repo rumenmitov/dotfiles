b1dcbb2feb6a463c62a44837e3d6dc9744d4b8d1
Rumen <rumenmitov@protonmail.com>
2026-03-07 20:07:01 +0100

    scripts: removing text from wallpaper.sh

--- .local/share/scripts/wallpaper.sh
+++ .local/share/scripts/wallpaper.sh
@@ -3,13 +3,16 @@
 wallpapersdir="$HOME/.local/share/wallpapers/Static"
 
 wallpaper=$( ls "$wallpapersdir"                                    \
-  | xargs -I{} printf "img:%s/%s:text:%s\n" "$wallpapersdir" {} {}  \
-  | wofi --dmenu --columns 2                                        \
+  | xargs -I{} printf "img:%s/%s\n" "$wallpapersdir" {}             \
+  | wofi --dmenu --columns 3                                        \
+    --define "x=50px"                                               \
+    --define "y=25px"                                               \
     --define "width=1200px"                                         \
     --define "height=800px"                                         \
     --define "image_size=300"                                       \
     --allow-images                                                  \
-  | awk -F: '{ print $NF }' )
+  | awk -F: '{ print $NF }'                                         \
+  | xargs basename)
 
 test -z "$wallpaper" && exit
 
